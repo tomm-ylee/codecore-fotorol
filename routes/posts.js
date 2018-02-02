88f65e9fbdd1af82d052ce8e02296c91efdbe38f
Tommy L <tommykwlee@gmail.com>
2018-02-02 12:18:44 -0800

    Add ability to show individual post

--- routes/posts.js
+++ routes/posts.js
@@ -27,7 +27,7 @@ router.post('', (req, res) => {
       // Database queries with knex are asynchronous, like setTimeout and
       // setInterval. If you want to write ANY CODE that depends on results
       // from a query, you must do it inside of the callback for 'then'.
-      res.redirect('/');
+      res.redirect('/posts');
     });
 
 });
@@ -38,11 +38,33 @@ router.get('', (req, res) => {
   knex
     .select()
     .from('posts')
+    .orderBy('created_at', 'DESC')
     // In this then, variable posts is the return of everything previous. ie,
-    // all the returned rows from the query.
+    // all the returned rows from the query. NOTE, posts here is an array of objects.
     .then(posts => {
-      res.send(posts); // Good execution during developmnent
+      // res.send(posts); // Good execution during developmnent
+      res.render('posts/index', {posts});
     });
+});
+
+// Posts#show PATH: /posts/:id VERB: GET
+// Display a single post from the db
+router.get('/:id', (req, res, next) => {
+  // Paths that have ':' in their name will be stored as a key value in req.params
+  // 'params' is a keyword. Use it to get the 'id' of a post.
+  console.log(req.params);
+  const postId = req.params.id;
+
+  if (isNaN(parseInt(postId, 10))) res.redirect('/');
+
+  knex
+    .first()
+    .from('posts')
+    .where({id: postId})
+    .then(post => {
+      res.render('posts/show', {post});
+    })
+
 });
 
 
